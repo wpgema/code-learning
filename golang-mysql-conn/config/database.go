c042d88e1b8d13864cdc75815c292eb531940a94
wpgema <mgemamaulana04@gmail.com>
2025-12-08 08:18:56 +0700

    Menambahkan file .env dan memperbarui koneksi database untuk menggunakan variabel lingkungan

--- golang-mysql-conn/config/database.go
+++ golang-mysql-conn/config/database.go
@@ -3,14 +3,41 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func ConnectDB() {
-	dsn := "root:@tcp(127.0.0.1:3306)/dbgolang"
+	// Muat file .env jika ada; jika tidak ada, lanjutkan menggunakan env system
+	_ = godotenv.Load()
+
+	// Ambil env, jika kosong gunakan default
+	user := os.Getenv("DB_USER")
+	if user == "" {
+		user = "root" // default user
+	}
+
+	pass := os.Getenv("DB_PASS") // default kosong
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = "127.0.0.1"
+	}
+
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		port = "3306"
+	}
+
+	name := os.Getenv("DB_NAME")
+	if name == "" {
+		name = "dbgolang" // default nama database
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, pass, host, port, name)
 
 	database, err := sql.Open("mysql", dsn)
 	if err != nil {
--- golang-mysql-conn/go.mod
+++ golang-mysql-conn/go.mod
@@ -5,6 +5,7 @@ go 1.25.5
 require (
 	github.com/gin-gonic/gin v1.11.0
 	github.com/go-sql-driver/mysql v1.9.3
+	github.com/joho/godotenv v1.5.1
 )
 
 require (
